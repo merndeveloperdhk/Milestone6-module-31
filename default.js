0a368ae2c8156f08bcac71bcaf5fdc02cc00a2c9
merndeveloperdhke <merndevelpler@gmail.comm>
2023-05-16 16:57:00 +0600

    third commit

--- default.js
+++ default.js
@@ -1,6 +1,24 @@
 function add(first, second){
+    second = second || 0;
+    // if(second === undefined){
+    //     second = 0;
+    // }
     const total = first + second;
     return total;
 }
-const result = add(25,50)
-console.log(result);
+const result = add(25)
+console.log("Total sum = ",result);
+
+function sum(first, second){
+    const total = first + second;
+    return total
+}
+const result1 = sum(20, 15);
+console.log(result1);
+
+function multiply(first, second){
+    const multiply = first * second;
+    return multiply;
+}
+const multiplyResult = multiply(5,7)
+console.log(multiplyResult);
---
+++ template.js
@@ -0,0 +1,16 @@
+
+const first = "Misam ";
+const second = `Marifa is
+a good girl`;
+const third = "Fatiha reads in class tEn"
+const join = first + second + third
+console.log(join);
+
+const a = 20;
+const b = 30;
+const numbs = [25,15,65,48,65,28]
+const numCount = numbs.length
+const summery = `amount of ${a} and amount of ${b} and abount of numbs ${numbs.length}`
+const total = a + b + numCount
+console.log(summery);
+console.log(total);
